bfd8d6a9b393d8a8f0a19778e43f425d0370c302
matt <matt@matts-Mac.local>
2024-10-14 21:23:41 -0700

    empty xcode project

---
+++ nutty_jump/nutty_jump/nutty_jumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  nutty_jumpApp.swift
+//  nutty_jump
+//
+//  Created by matt on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct nutty_jumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nutty_jump/nutty_jumpTests/nutty_jumpTests.swift
@@ -0,0 +1,16 @@
+//
+//  nutty_jumpTests.swift
+//  nutty_jumpTests
+//
+//  Created by matt on 10/14/24.
+//
+
+import Testing
+
+struct nutty_jumpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
